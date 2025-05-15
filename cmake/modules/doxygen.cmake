14ac7d034dfc985a6266d098fc2e4e597a21efcd
Marius Johannes Winkler <mariusjwinkler@gmail.com>
2025-05-15 18:21:46 +0200

    moved doxygen configuration into doxygen.cmake

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -3,3 +3,14 @@
 # TODO: create CMake build option for the target.
 
 # TODO: Add a custom target for building the documentation.
+
+#Check if Doxygen is installed
+find_package(Doxygen)
+if (DOXYGEN_FOUND)
+    # create doxygen target
+    add_custom_target(doc_doxygen
+            COMMAND ${DOXYGEN_EXECUTABLE} ${PROJECT_SOURCE_DIR}/Doxyfile
+            COMMENT "Building HTML Documentation")
+else ()
+    message(STATUS "Doxygen needs to be installed to generate the doxygen documentation.")
+endif (DOXYGEN_FOUND)
